bbc6ace9bfcb7189f7d47a0a6dc4176796e2a0da
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2020-10-29 14:21:41 -0400

    Added firebase

---
+++ pages/firebase.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAhTZe0TmRfYZCP7Cea0Nc6e-r76QRWc4A",
+    authDomain: "joesite-61715.firebaseapp.com",
+    databaseURL: "https://joesite-61715.firebaseio.com",
+    projectId: "joesite-61715",
+    storageBucket: "joesite-61715.appspot.com",
+    messagingSenderId: "641166144810",
+    appId: "1:641166144810:web:acabfe1337165e0ca19a53"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
